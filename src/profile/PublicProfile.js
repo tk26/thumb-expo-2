c913c8d1fcbdad372ef8d19e1d20ffa23709efaa
Tejas Kumthekar <tk@Tejass-MacBook-Pro.local>
2018-06-19 00:47:45 -0500

    Add follow user

--- src/profile/PublicProfile.js
+++ src/profile/PublicProfile.js
@@ -3,7 +3,8 @@ import { View, Text, Image, Button } from 'react-native';
 import { getApiUrl } from '.././helper';
 
 const initialState = {
-    firstName: '', lastName: '', school: '', profilePicture: '', error: ''
+    firstName: '', lastName: '', school: '', profilePicture: '', 
+    followsMe: false, followedByMe: false, error: ''
 }
 
 export default class PublicProfile extends Component {
@@ -31,8 +32,43 @@ export default class PublicProfile extends Component {
                     firstName: response.firstName,
                     lastName: response.lastName,
                     school: response.school,
-                    profilePicture: response.profilePicture
+                    profilePicture: response.profilePicture,
+                    followsMe: response.follows.find( _ => _.username === global.username),
+                    followedByMe: response.followedBy.find( _ => _.username === global.username),
                 });
+            }
+            else {
+                this.setState({
+                    error: "Some error occured. Please try again. If problem persists, " +
+                        "please let us know at support@thumbtravel.com"
+                });
+            }
+        }).catch(error => {
+            // TODO log error
+            this.setState({
+                error: "Some error occured. Please try again. If problem persists, " +
+                    "please let us know at support@thumbtravel.com"
+            });
+        });
+    }
+
+    followUser() {
+        let responseStatus = 0;
+        fetch(getApiUrl() + '/user/follow/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer' + ' ' + global.auth_token
+            },
+            body: JSON.stringify({
+                "toUsername" : this.state.username,
+            })
+        }).then( response => {
+            responseStatus = response.status;
+            return response.json();
+        }).then( response => {
+            if (responseStatus === 200) {
+                this.setState({ followedByMe: true });
             }
             else {
                 this.setState({
@@ -66,10 +102,19 @@ export default class PublicProfile extends Component {
                     {this.state.lastName}
                     {'\n'}
                     {this.state.school}
+                    {'\n'}
+                    {this.state.followsMe ? "Follows You" : ""}
                 </Text>
+
+                <Button
+                    disabled= {this.state.followedByMe ? true : false} //had to resolve to boolean to avoid nasty warnings
+                    onPress={() => this.followUser()}
+                    title={this.state.followedByMe ? "Following" : "Follow"}
+                />
+
                 <Button
                     onPress={() => this.props.navigation.goBack()}
-                    title = "Dismiss"
+                    title="Dismiss"
                 />
                 <Text>{this.state.error}</Text>
             </View>
