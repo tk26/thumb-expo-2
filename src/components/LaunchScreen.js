74a9badcb86f9843d41ee707487ac02a60b0df6b
jdromine <jromines@gmail.com>
2018-09-07 17:45:46 -0400

    THUM-234 - Adding helvetic neue fonts for android devices

--- src/components/LaunchScreen.js
+++ src/components/LaunchScreen.js
@@ -11,7 +11,7 @@ export default class LaunchScreen extends Component {
                 <Button title="Login" onPress={() => this.props.navigation.navigate('LoginScreen')} />
                 <Button title="Create Account" onPress={() => this.props.navigation.navigate('SignupStep1')}/>
                 
-                <Text>
+                <Text style={{fontFamily: 'Roboto'}}>
                     By tapping Log In or Create Account, I agree to thumb's&nbsp;
                     <Text style={{ color: 'blue' }} onPress={() => Linking.openURL('https://www.google.com')}>
                         Terms of Service
