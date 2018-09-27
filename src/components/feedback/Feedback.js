246bb6af6e8c0b4283395011d57946845985b78e
jdromine <jromines@gmail.com>
2018-09-27 07:23:13 -0400

    Adding build script

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -9,7 +9,7 @@ export default class Feedback extends Component {
         this.state = initialState;
     }
 
-    showFeedbackSubmitMessage = (message) => {
+    showFeedbackSubmitMessage(message){
         this.setState({ feedbackSubmitMessage: message });
     }
 
@@ -21,9 +21,9 @@ export default class Feedback extends Component {
                 />
 
                 <Text>{ this.state.feedbackSubmitMessage } {'\n'}</Text>
-                
+
                 <Text>how are we doing?{'\n'}</Text>
-                
+
                 <Text>
                     We want to hear what you love and what you think we can do better.
                     We also want to integrate your suggestions to make our product better in the future.
@@ -31,41 +31,41 @@ export default class Feedback extends Component {
                     {'\n'}
                 </Text>
 
-                
+
                 <Text>
                     If you have a question or need help resolving an issue quickly, please email
                     info@thumbtravel.com, or visit our FAQ's page. We will get back to you as soon as possible.
                     {'\n'}
                 </Text>
-        
 
-            
+
+
                 <Text>
                     WHAT WOULD YOU LIKE TO DO?
                     {'\n'}
                 </Text>
 
                     <Button title="Report a bug" style={{ alignSelf: 'center' }}
-                        onPress={() => this.props.navigation.navigate('ReportBug', { 
-                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage 
+                        onPress={() => this.props.navigation.navigate('ReportBug', {
+                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage
                             }) }
                     />
 
                     <Button title="Ask a question" rounded info style={{ alignSelf: 'center' }}
-                        onPress={() => this.props.navigation.navigate('AskQuestion', { 
-                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage 
+                        onPress={() => this.props.navigation.navigate('AskQuestion', {
+                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage
                             }) }
                     />
 
                     <Button title="Request a feature" style={{ alignSelf: 'center' }}
-                        onPress={() => this.props.navigation.navigate('RequestFeature', { 
-                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage 
+                        onPress={() => this.props.navigation.navigate('RequestFeature', {
+                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage
                             }) }
                     />
 
                     <Button title="Other" rounded info style={{ alignSelf: 'center' }}
-                        onPress={() => this.props.navigation.navigate('OtherFeedback', { 
-                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage 
+                        onPress={() => this.props.navigation.navigate('OtherFeedback', {
+                                showFeedbackSubmitMessage: this.showFeedbackSubmitMessage
                             }) }
                     />
             </View>
