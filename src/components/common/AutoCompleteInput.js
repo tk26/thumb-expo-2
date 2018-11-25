5d7219d7f0ae1ec527bb5b12951a3ad8d866d4a9
jdromine <jromines@gmail.com>
2018-11-25 16:07:07 -0500

    Fixing IOS display issues

--- src/components/common/AutoCompleteInput.js
+++ src/components/common/AutoCompleteInput.js
@@ -44,6 +44,18 @@ export class AutoCompleteInput extends Component {
             inputContainerStyle={styles.inputContainer}
             listStyle={styles.list}
             data={items.length === 1 && comp(query, items[0][filterProperty]) ? [] : items}
+            renderTextInput={() => {
+              return (
+                <TextInput
+                  placeholder={placeholder}
+                  onChangeText={text => this.setState({ query: text })}
+                  value={this.state.query}
+                  underlineColorAndroid='transparent'
+                  placeholderTextColor={fontColors.grey}
+                  style={styles.searchInput}
+                />
+              )}
+            }
             renderItem={(item) => (
               <CardSection>
                 <TouchableOpacity onPress={() => {
@@ -74,6 +86,14 @@ const styles = {
     right: 0,
     top: 0,
     borderWidth: 0
+  },
+  searchInput: {
+    color: fontColors.lightGrey,
+    fontSize: 14,
+    fontFamily: 'Helvetica Neue',
+    letterSpacing: 0,
+    lineHeight: 23,
+    flex: 1
   },
   inputContainer: {
     paddingLeft: 5,
